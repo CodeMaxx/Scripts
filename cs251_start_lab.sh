21e5678ef365d5cf089e02d29efc101f4a1355fe
Akash Trehan <akash.trehan123@gmail.com>
2016-10-10 04:26:12 +0530

    Unzip files

--- cs251_start_lab.sh
+++ cs251_start_lab.sh
@@ -6,4 +6,7 @@ touch lab$1/inlab/readme.txt;
 touch lab$1/outlab/readme.txt;
 cd lab$1/resources;
 wget -r -np -nH --cut-dirs=5 -R index.html* https://www.cse.iitb.ac.in/~sharat/current/cs251/Assign/Lab0$1/;
+for i in `find . -type f -name *.tgz | sed -E 's/\.\/(a*)/\1/'`; do tar -xvf $i; done
+for i in `find . -type f -name *.tar.gz | sed -E 's/\.\/(a*)/\1/'`; do tar -xvf $i; done
+for i in `find . -type f -name *.zip | sed -E 's/\.\/(a*)/\1/'`; do unzip $i; done
 rm robots.txt;
